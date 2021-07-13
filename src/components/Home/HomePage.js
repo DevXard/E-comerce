7bb7f14dd011e1a571f824f0b40740a1726728b7
Pavlin <forex.xard@gmail.com>
2021-07-13 18:41:06 -0400

    Adding sample layout

---
+++ src/components/Home/HomePage.js
@@ -0,0 +1,43 @@
+import './homepage.styles.scss'
+
+const HomePage = () => {
+
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Hats</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Jackets</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Snickers</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Womens</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">Man's</h1>
+                        <span className="subtitle">Shop Now</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage;
